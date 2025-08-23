ad3afe02ab6ad10a8c92fd721ab02adeaa4da971
Doeunkim31004 <jenny4262@naver.com>
2025-08-23 17:46:12 +0900

    kde

---
+++ commutestress/src/main/java/org/zerock/commutestress/CommutestressApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.commutestress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutestressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommutestressApplication.class, args);
+	}
+
+}
